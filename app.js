e1138659201cf0940528ca2d75e96e650c1f6a4c
NickWilde18 <james_yyc@outlook.com>
2023-03-04 14:06:11 +0800

    Change to Woff2 fonts

--- app.js
+++ app.js
@@ -1,21 +1,6 @@
 // app.js
 App({
-      globalData:{
-        key:"key",
-      },
       onLaunch() {
-        // 展示本地存储能力
-        //const logs = wx.getStorageSync('logs') || [],
-        //logs.unshift(Date.now()),
-        //wx.setStorageSync('logs', logs),
-        wx.loadFontFace({
-          family:'STZHONGS',
-        //  source: 'url("https://1708v513b2.goho.co/STZHONGS.TTF")',
-          source:'url("https://7869-xinhuocloud-9gyb31w57af6eb86-1317102184.tcb.qcloud.la/STZhongsong.woff?sign=9cae6d22f1339265cbdfa6069d3b3029&t=1677853211")',
-          success:console.log,
-          fail:console.log,
-          global:'true',
-        })
       },
     
 })
